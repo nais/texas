58dd869ee834791261873b12f1e38caf380327c5
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-10-28 09:50:49 +0100

    feat: add basic config parsing
    Co-Authored-By: Kim Tore Jensen <kim.tore.jensen@nav.no>
    Co-Authored-By: Tommy Trøen <tommy.troen@nav.no>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+use dotenv::dotenv;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long, env = "TEXAS_NAME")]
+    texas_name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = dotenv(); // load .env if present
+
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Howdy, {}!", args.texas_name);
+    }
 }
