334fa880a0a317725ce08d76827f060731b50460
Kim Tore Jensen <kimtjen@gmail.com>
2024-10-28 15:13:03 +0100

    feat: it's not Friday yet
    Co-authored-by: Trong Huu Nguyen <trong.huu.nguyen@nav.no>
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,28 @@ struct Config {
     maskinporten_token_endpoint: String,
 }
 
+fn print_texas_logo() {
+    info!(r#"      ____"#);
+    info!(r#"           !"#);
+    info!(r#"     !     !"#);
+    info!(r#"     !      `-  _ _    _ "#);
+    info!(r#"     |              ```  !      _"#);
+    info!(r#"_____!                   !     | |"#);
+    info!(r#"\,                        \    | |_ _____  ____ _ ___"#);
+    info!(r#"  l    _                  ;    | __/ _ \ \/ / _` / __|"#);
+    info!(r#"   \ _/  \.              /     | ||  __/>  < (_| \__ \"#);
+    info!(r#"           \           .’       \__\___/_/\_\__,_|___/"#);
+    info!(r#"            .       ./’"#);
+    info!(r#"             `.    ,"#);
+    info!(r#"               \   ;"#);
+    info!(r#"                 ``’"#);
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::builder().filter_level(LevelFilter::Debug).init();
 
-    info!("TEXAS starting");
+    print_texas_logo();
 
     let _ = dotenv(); // load .env if present
 
