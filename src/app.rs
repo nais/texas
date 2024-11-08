2eea7c47d969925f48465b97f8d01cbaf50c3f0f
Tommy Trøen <tommy.troen@nav.no>
2024-11-08 13:54:10 +0100

    tests: add more tests
    Co-authored-by: tronghn <trong.huu.nguyen@nav.no>

--- src/app.rs
+++ src/app.rs
@@ -144,6 +144,9 @@ mod tests {
         introspect_token_is_not_a_jwt(&address).await;
         test_introspect_missing_issuer(&address).await;
         test_introspect_unrecognized_issuer(&address).await;
+        introspect_token_missing_kid(&address).await;
+        introspect_token_missing_key_in_jwks(&address).await;
+        introspect_token_invalid_timestamps(&address).await;
 
         // TODO: implement these tests:
         // * /token
@@ -165,9 +168,8 @@ mod tests {
         //   * [x] token is not a jwt
         //   * [x] token does not contain iss claim
         //   * [x] token is issued by unrecognized issuer
-        //   * [ ] token has invalid header
-        //   * [ ] token does not have kid (key id) in header
-        //   * [ ] token is signed with a key that is not in the jwks
+        //   * [x] token does not have kid (key id) in header
+        //   * [x] token is signed with a key that is not in the jwks
         //   * [ ] invalid or expired timestamps in nbf, iat, exp
         //   * [ ] invalid or missing aud (for certain providers)
         //   * [ ] refreshing jwks fails
@@ -179,72 +181,93 @@ mod tests {
     }
 
     async fn test_introspect_unrecognized_issuer(address: &str) {
-        let token = sign_with_default_jwk(
-            HashMap::<String, Value>::from([
+        let token = TokenServer::token(
+            TokenClaims::from([
                 ("iss".into(), Value::String("snafu".into())),
             ])
         );
         test_well_formed_json_request(
+            &format!("http://{}/api/v1/introspect", address),
             IntrospectRequest {
                 token,
             },
             IntrospectResponse::new_invalid("token has unknown issuer: snafu"),
-            &format!("http://{}/api/v1/introspect", address),
             StatusCode::OK,
         ).await;
     }
 
     async fn test_introspect_missing_issuer(address: &str) {
-        let token = sign_with_default_jwk(HashMap::<String, Value>::new());
+        let token = TokenServer::token(TokenClaims::new());
         test_well_formed_json_request(
+            &format!("http://{}/api/v1/introspect", address),
             IntrospectRequest {
                 token,
             },
             IntrospectResponse::new_invalid("JSON error: missing field `iss` at line 1 column 2"),
-            &format!("http://{}/api/v1/introspect", address),
             StatusCode::OK,
         ).await;
     }
 
-    #[cfg(test)]
-    struct TokenServer {
-        listener: tokio::net::TcpListener,
-        keys: HashMap<String, Vec<jwk::JsonWebKey>>,
+    async fn introspect_token_is_not_a_jwt(address: &str) {
+        test_well_formed_json_request(
+            &format!("http://{}/api/v1/introspect", address),
+            IntrospectRequest {
+                token: "this is not a token".to_string(),
+            },
+            IntrospectResponse::new_invalid("InvalidToken"),
+            StatusCode::OK,
+        ).await;
     }
 
-    #[cfg(test)]
-    impl TokenServer {
-        async fn new() -> Self {
-            let mut keys = HashMap::<String, Vec<jwk::JsonWebKey>>::new();
-            keys.insert("keys".to_string(), vec![get_signing_key()]);
-            Self {
-                listener: tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap(),
-                keys,
-            }
-        }
-
-        fn address(&self) -> String {
-            self.listener.local_addr().unwrap().to_string()
-        }
-
-        pub async fn run(self) -> std::io::Result<()> {
-            let app = Router::new()
-                .route("/token", get(|| async {
-                    "Hello, world!"
-                }))
-                .route("/jwks", get(|| async { Json(self.keys) }));
-
-            axum::serve(self.listener, app).await
-        }
+    async fn introspect_token_missing_kid(address: &str) {
+        let token = TokenServer::token(
+            TokenClaims::from([
+                ("iss".into(), Value::String("http://localhost:8080/maskinporten".into())),
+            ])
+        );
+        test_well_formed_json_request(
+            &format!("http://{}/api/v1/introspect", address),
+            IntrospectRequest {
+                token,
+            },
+            IntrospectResponse::new_invalid("missing key id from token header"),
+            StatusCode::OK,
+        ).await;
     }
 
-    #[tokio::test]
-    async fn foo() {
+    async fn introspect_token_missing_key_in_jwks(address: &str) {
+        let token = TokenServer::token_with_kid(
+            TokenClaims::from([
+                ("iss".into(), Value::String("http://localhost:8080/maskinporten".into())),
+            ]),
+            "missing-key",
+        );
+
+        test_well_formed_json_request(
+            &format!("http://{}/api/v1/introspect", address),
+            IntrospectRequest {
+                token,
+            },
+            IntrospectResponse::new_invalid("signing key with missing-key not in json web key set"),
+            StatusCode::OK,
+        ).await;
+    }
+    //   * [ ] invalid or expired timestamps in nbf, iat, exp
+    async fn introspect_token_invalid_timestamps(address: &str) {
+
+    }
+
+    async fn yolo() {
         let server = TokenServer::new().await;
         let address = server.address();
         let join_handler = tokio::spawn(async move {
             server.run().await.unwrap();
         });
+
+        // Set up Texas
+        //let cfg = Config::mock(address, server.port());
+        //let app = App::new(cfg.clone()).await;
+
 
         let client = reqwest::Client::new();
         let response = client.get(format!("http://{}/jwks", address)).send().await.unwrap();
@@ -252,35 +275,79 @@ mod tests {
         join_handler.abort();
     }
 
-
-    fn get_signing_key() -> jwk::JsonWebKey {
-        let raw = r#"{"p":"_LNnIjBshCrFuxtjUC2KKzg_NTVv26UZh5j12_9r5mYTxb8yW047jOYFEGvIdMkTRLGOBig6fLWzgd62lnLainzV35J6K6zr4jQfTldLondlkldMR6nQrp1KfnNUuRbKvzpNKkhl12-f1l91l0tCx3s4blztvWgdzN2xBfvWV68","kty":"RSA","q":"9MIWsbIA3WjiR_Ful5FM8NCgb6JdS2D6ySHVepoNI-iAPilcltF_J2orjfLqAxeztTskPi45wtF_-eV4GIYSzvMo-gFiXLMrvEa7WaWizMi_7Bu9tEk3m_f3IDLN9lwULYoebkDbiXx6GOiuj0VkuKz8ckYFNKLCMP9QRLFff-0","d":"J6UX848X8tNz-09PFvcFDUVqak32GXzoPjnuDjBsxNUvG7LxenLmM_i8tvYl0EW9Ztn4AiCqJUoHw5cX3jz_mSqGl7ciaDedpKm_AetcZwHiEuT1EpSKRPMmOMQSqcJqXrdbbWB8gdUrnTKZIlJCfj7yqgT16ypC43TnwjA0UwxhG5pHaYjKI3pPdoHg2BzA-iubHjVn15Sz7-pnjBmeGDbEFa7ADY-1yPHCmqqvPKTNhoCNW6RpG34Id9hXslPa3X-7pAhJrDBd0_NPlktSA2rUkifYiZURhHR5ijhe0v3uw6kYP8f_foVm_C8O1ExkxXh9Dg8KDZ89dbsSOtBc0Q","e":"AQAB","use":"sig","kid":"l7C_WJgbZ_6e59vPrFETAehX7Dsp7fIyvSV4XhotsGs","qi":"cQFN5q5WhYkzgd1RS0rGqvpX1AkmZMrLv2MW04gSfu0dDwpbsSAu8EUCQW9oA4pr6V7R9CBSu9kdN2iY5SR-hZvEad5nDKPV1F3TMQYv5KpRiS_0XhfV5PcolUJVO_4p3h8d-mo2hh1Sw2fairAKOzvnwJCQ6DFkiY7H1cqwA54","dp":"YTql9AGtvyy158gh7jeXcgmySEbHQzvDFulDr-IXIg8kjHGEbp0rTIs0Z50RA95aC5RFkRjpaBKBfvaySjDm5WIi6GLzntpp6B8l7H6qG1jVO_la4Df2kzjx8LVvY8fhOrKz_hDdHodUeKdCF3RdvWMr00ruLnJhBPJHqoW7cwE","alg":"RS256","dq":"IZA4AngRbEtEtG7kJn6zWVaSmZxfRMXwvgIYvy4-3Qy2AVA0tS3XTPVfMaD8_B2U9CY_CxPVseR-sysHc_12uNBZbycfcOzU84WTjXCMSZ7BysPnGMDtkkLHra-p1L29upz1HVNhh5H9QEswHM98R2LZX2ZAsn4bORLZ1AGqweU","n":"8ZqUp5Cs90XpNn8tJBdUUxdGH4bjqKjFj8lyB3x50RpTuECuwzX1NpVqyFENDiEtMja5fdmJl6SErjnhj6kbhcmfmFibANuG-0WlV5yMysdSbocd75C1JQbiPdpHdXrijmVFMfDnoZTQ-ErNsqqngTNkn5SXBcPenli6Cf9MTSchZuh_qFj_B7Fp3CWKehTiyBcLlNOIjYsXX8WQjZkWKGpQ23AWjZulngWRektLcRWuEKTWaRBtbAr3XAfSmcqTICrebaD3IMWKHDtvzHAt_pt4wnZ06clgeO2Wbc980usnpsF7g8k9p81RcbS4JEZmuuA9NCmOmbyADXwgA9_-Aw"}"#;
-        raw.parse().unwrap()
+    struct TokenServer {
+        listener: tokio::net::TcpListener,
+        keys: HashMap<String, Vec<jwk::JsonWebKey>>,
     }
 
-    fn sign_with_default_jwk(claims: impl Serialize) -> String {
-        let key = get_signing_key();
-        let alg: jwt::Algorithm = key.algorithm.unwrap().into();
-        jwt::encode(
-            &jwt::Header::new(alg),
-            &claims,
-            &key.key.to_encoding_key(),
-        ).unwrap()
+    type TokenClaims = HashMap<String, Value>;
+
+    struct Token {
+        header: jwt::Header,
+        claims: TokenClaims,
     }
 
-    async fn introspect_token_is_not_a_jwt(address: &str) {
-        test_well_formed_json_request(
-            IntrospectRequest {
-                token: "this is not a token".to_string(),
-            },
-            IntrospectResponse::new_invalid("InvalidToken"),
-            &format!("http://{}/api/v1/introspect", address),
-            StatusCode::OK,
-        ).await;
+    impl Token {
+        fn new(claims: TokenClaims) -> Self {
+            Self {
+                header: jwt::Header::new(jwt::Algorithm::RS256),
+                claims,
+            }
+        }
+
+        fn sign(self, key: &jwk::JsonWebKey) -> String {
+            jwt::encode(
+                &self.header,
+                &self.claims,
+                &key.key.to_encoding_key(),
+            ).unwrap()
+        }
+    }
+
+    impl TokenServer {
+        const SIGNING_KEY: &'static str = r#"{"p":"_LNnIjBshCrFuxtjUC2KKzg_NTVv26UZh5j12_9r5mYTxb8yW047jOYFEGvIdMkTRLGOBig6fLWzgd62lnLainzV35J6K6zr4jQfTldLondlkldMR6nQrp1KfnNUuRbKvzpNKkhl12-f1l91l0tCx3s4blztvWgdzN2xBfvWV68","kty":"RSA","q":"9MIWsbIA3WjiR_Ful5FM8NCgb6JdS2D6ySHVepoNI-iAPilcltF_J2orjfLqAxeztTskPi45wtF_-eV4GIYSzvMo-gFiXLMrvEa7WaWizMi_7Bu9tEk3m_f3IDLN9lwULYoebkDbiXx6GOiuj0VkuKz8ckYFNKLCMP9QRLFff-0","d":"J6UX848X8tNz-09PFvcFDUVqak32GXzoPjnuDjBsxNUvG7LxenLmM_i8tvYl0EW9Ztn4AiCqJUoHw5cX3jz_mSqGl7ciaDedpKm_AetcZwHiEuT1EpSKRPMmOMQSqcJqXrdbbWB8gdUrnTKZIlJCfj7yqgT16ypC43TnwjA0UwxhG5pHaYjKI3pPdoHg2BzA-iubHjVn15Sz7-pnjBmeGDbEFa7ADY-1yPHCmqqvPKTNhoCNW6RpG34Id9hXslPa3X-7pAhJrDBd0_NPlktSA2rUkifYiZURhHR5ijhe0v3uw6kYP8f_foVm_C8O1ExkxXh9Dg8KDZ89dbsSOtBc0Q","e":"AQAB","use":"sig","kid":"l7C_WJgbZ_6e59vPrFETAehX7Dsp7fIyvSV4XhotsGs","qi":"cQFN5q5WhYkzgd1RS0rGqvpX1AkmZMrLv2MW04gSfu0dDwpbsSAu8EUCQW9oA4pr6V7R9CBSu9kdN2iY5SR-hZvEad5nDKPV1F3TMQYv5KpRiS_0XhfV5PcolUJVO_4p3h8d-mo2hh1Sw2fairAKOzvnwJCQ6DFkiY7H1cqwA54","dp":"YTql9AGtvyy158gh7jeXcgmySEbHQzvDFulDr-IXIg8kjHGEbp0rTIs0Z50RA95aC5RFkRjpaBKBfvaySjDm5WIi6GLzntpp6B8l7H6qG1jVO_la4Df2kzjx8LVvY8fhOrKz_hDdHodUeKdCF3RdvWMr00ruLnJhBPJHqoW7cwE","alg":"RS256","dq":"IZA4AngRbEtEtG7kJn6zWVaSmZxfRMXwvgIYvy4-3Qy2AVA0tS3XTPVfMaD8_B2U9CY_CxPVseR-sysHc_12uNBZbycfcOzU84WTjXCMSZ7BysPnGMDtkkLHra-p1L29upz1HVNhh5H9QEswHM98R2LZX2ZAsn4bORLZ1AGqweU","n":"8ZqUp5Cs90XpNn8tJBdUUxdGH4bjqKjFj8lyB3x50RpTuECuwzX1NpVqyFENDiEtMja5fdmJl6SErjnhj6kbhcmfmFibANuG-0WlV5yMysdSbocd75C1JQbiPdpHdXrijmVFMfDnoZTQ-ErNsqqngTNkn5SXBcPenli6Cf9MTSchZuh_qFj_B7Fp3CWKehTiyBcLlNOIjYsXX8WQjZkWKGpQ23AWjZulngWRektLcRWuEKTWaRBtbAr3XAfSmcqTICrebaD3IMWKHDtvzHAt_pt4wnZ06clgeO2Wbc980usnpsF7g8k9p81RcbS4JEZmuuA9NCmOmbyADXwgA9_-Aw"}"#;
+        async fn new() -> Self {
+            let mut keys = HashMap::<String, Vec<jwk::JsonWebKey>>::new();
+            keys.insert("keys".to_string(), vec![Self::get_signing_key()]);
+            Self {
+                listener: tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap(),
+                keys,
+            }
+        }
+
+        fn port(&self) -> u16 {
+            self.listener.local_addr().unwrap().port()
+        }
+
+        fn address(&self) -> String {
+            self.listener.local_addr().unwrap().to_string()
+        }
+
+        fn token(claims: TokenClaims) -> String {
+            Token::new(claims).sign(&Self::get_signing_key())
+        }
+
+        fn token_with_kid(claims: TokenClaims, kid: &str) -> String {
+            let mut token = Token::new(claims);
+            token.header.kid = Some(kid.to_string());
+            token.sign(&Self::get_signing_key())
+        }
+
+        pub async fn run(self) -> std::io::Result<()> {
+            let app = Router::new()
+                .route("/jwks", get(|| async { Json(self.keys) }));
+
+            axum::serve(self.listener, app).await
+        }
+
+        fn get_signing_key() -> jwk::JsonWebKey {
+            Self::SIGNING_KEY.parse().unwrap()
+        }
     }
 
     async fn missing_or_empty_user_token(address: &str) {
         test_well_formed_json_request(
+            &format!("http://{}/api/v1/token/exchange", address),
             TokenExchangeRequest {
                 target: "target".to_string(),
                 identity_provider: IdentityProvider::AzureAD,
@@ -290,7 +357,6 @@ mod tests {
                 error: OAuthErrorCode::InvalidRequest,
                 description: "invalid request: missing or empty assertion parameter".to_string(),
             },
-            &format!("http://{}/api/v1/token/exchange", address),
             StatusCode::BAD_REQUEST,
         ).await;
     }
@@ -429,9 +495,9 @@ mod tests {
     }
 
     async fn test_well_formed_json_request<T: Serialize, U: DeserializeOwned + PartialEq + Debug>(
+        url: &str,
         request: T,
         response: U,
-        url: &str,
         status_code: StatusCode,
     ) {
         let http_response = post_request(
