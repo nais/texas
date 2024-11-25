35348637892bb336b4a5341a4e88f5f8ae14a3ea
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-11-25 14:14:48 +0100

    fix: accidentally a bracket

--- hack/roundtrip-azure-cc.sh
+++ hack/roundtrip-azure-cc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-response=$(curl -s -X POST http://localhost:3000/api/v1/token -H "content-type: application/json" -d '{"target": "client-id", "identity_provider": "azuread"')
+response=$(curl -s -X POST http://localhost:3000/api/v1/token -H "content-type: application/json" -d '{"target": "client-id", "identity_provider": "azuread"}')
 token=$(echo ${response} | jq -r .access_token)
 
 validation=$(curl -s -X POST http://localhost:3000/api/v1/introspect -H "content-type: application/json" -d "{\"token\": \"${token}\", \"identity_provider\": \"azuread\"}")
