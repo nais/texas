fa50fe2e5b401e09478de5c206bbc3f8b7781899
Kim Tore Jensen <kimtjen@gmail.com>
2024-11-25 13:04:52 +0100

    fix: compile openapi exporter

--- src/export_openapi_spec.rs
+++ src/export_openapi_spec.rs
@@ -1,6 +1,5 @@
 use std::io::Write;
 use texas::app::App;
-use texas::config::DownstreamApp;
 use texas::handlers::HandlerState;
 
 /// Write the OpenAPI specification to standard output.
@@ -9,7 +8,8 @@ async fn main() {
     let mut stdout = std::io::stdout();
     let cfg = texas::config::Config::default();
     let token_cache = moka::future::Cache::new(0);
-    let (_, openapi) = App::routes(HandlerState{ cfg, token_cache, providers: vec![] }, DownstreamApp::default());
+    let token_exchange_cache = moka::future::Cache::new(0);
+    let (_, openapi) = App::routes(HandlerState{ cfg, token_cache, token_exchange_cache, providers: vec![] });
     let data = openapi.to_pretty_json().unwrap();
     stdout.write_all(data.as_bytes()).unwrap();
 }
