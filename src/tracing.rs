b473e7b7cdd4a1fe02eed76db60dc4908ee2c39f
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2025-09-08 09:54:17 +0200

    feat(tracing): add more buckets to latency histogram

--- src/tracing.rs
+++ src/tracing.rs
@@ -236,7 +236,8 @@ static HISTOGRAM_HTTP_RESPONSE_SECS: LazyLock<Histogram<f64>> = LazyLock::new(||
         .with_description("Response time in seconds")
         .with_boundaries(vec![
             0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.02, 0.03, 0.04,
-            0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
+            0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0,
+            5.0, 10.0,
         ])
         .build()
 });
