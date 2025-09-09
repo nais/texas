a2985949ca9e68ba66b2c471e7f06d7a4c6934db
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2025-09-09 18:04:56 +0200

    fix(http): set more reasonable defaults for connect and read timeouts

--- src/http.rs
+++ src/http.rs
@@ -10,8 +10,8 @@ pub fn new_client(retry: Retry) -> Result<ClientWithMiddleware, reqwest::Error> 
         .jitter(Jitter::None)
         .build_with_max_retries(retry.max_retries);
 
-    let connect_timeout_millis = env_or_default::<u64>("TEXAS_HTTP_CONNECT_TIMEOUT_MILLIS", 200);
-    let read_timeout_millis = env_or_default::<u64>("TEXAS_HTTP_READ_TIMEOUT_MILLIS", 500);
+    let connect_timeout_millis = env_or_default::<u64>("TEXAS_HTTP_CONNECT_TIMEOUT_MILLIS", 1_000);
+    let read_timeout_millis = env_or_default::<u64>("TEXAS_HTTP_READ_TIMEOUT_MILLIS", 1_000);
     let overall_timeout_millis = env_or_default::<u64>("TEXAS_HTTP_OVERALL_TIMEOUT_MILLIS", 1_000);
     let pool_max_idle = env_or_default::<usize>("TEXAS_HTTP_POOL_MAX_IDLE", 10_000);
 
